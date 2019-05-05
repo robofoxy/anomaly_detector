b356d03d6c21eddb73c8a78f7f8c6ab21e300e99
İsmail Tüzün <evliyali.tzn@gmail.com>
2019-05-05 09:19:49 +0000

    2. arg takes input vector file path, sends its content line by line

--- p4-src/send.py
+++ p4-src/send.py
@@ -8,6 +8,7 @@ import struct
 from scapy.all import sendp, send, get_if_list, get_if_hwaddr
 from scapy.all import Packet, Raw
 from scapy.all import Ether, IP, UDP, TCP
+from time import sleep
 
 def get_if():
     ifs=get_if_list()
@@ -24,17 +25,23 @@ def get_if():
 def main():
 
     if len(sys.argv)<3:
-        print 'pass 2 arguments: <destination> "<message>"'
+        print 'pass 2 arguments: <destination> "<input_path>"'
         exit(1)
 
     addr = socket.gethostbyname(sys.argv[1])
     iface = get_if()
-
     print "sending on interface %s to %s" % (iface, str(addr))
-    pkt =  Ether(src=get_if_hwaddr(iface),dst = "00:00:00:00:01:12")
-    pkt = pkt /IP(dst=addr)/TCP(dport=8080)/Raw(load=sys.argv[2])
-    pkt.show2()
-    sendp(pkt, iface=iface, verbose=False)
+
+    filepath = sys.argv[2]
+    with open(filepath) as fp:
+       line = fp.readline()
+       while line:
+           pkt =  Ether(src=get_if_hwaddr(iface),dst = "00:00:00:00:01:12")
+           pkt = pkt /IP(dst=addr)/TCP(dport=8080)/Raw(load=line.strip())
+           pkt.show2()
+           sendp(pkt, iface=iface, verbose=False)
+           sleep(0.05)
+           line = fp.readline()
 
 
 if __name__ == '__main__':
